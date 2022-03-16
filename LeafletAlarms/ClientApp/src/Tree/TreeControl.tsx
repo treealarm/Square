8604826ee38dc5fbb17e9156fea12244c135ca5a
danil.serov <danil.serov@graphicalnetworks.com>
2022-03-16 11:18:43 +0300

    feat: Tree->List

--- LeafletAlarms/ClientApp/src/Tree/TreeControl.tsx
+++ LeafletAlarms/ClientApp/src/Tree/TreeControl.tsx
@@ -2,12 +2,19 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { useEffect, useCallback, useState} from 'react';
-import { TreeItem, TreeView } from '@mui/lab';
-
 import { useDispatch, useSelector, useStore } from "react-redux";
 import * as TreeStore from '../store/TreeStates';
 import * as GuiStore from '../store/GUIStates';
 import { ApplicationState } from '../store';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
 
 declare module 'react-redux' {
   interface DefaultRootState extends ApplicationState { }
@@ -27,63 +34,73 @@ export function TreeControl() {
   const markers = useSelector((state) => state?.treeStates?.markers);
   const parent_id = useSelector((state) => state?.treeStates?.parent_id);
 
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [checked, setChecked] = React.useState([]);
 
-    const handleListItemClick = (event, index) => {
-      setSelectedIndex(index);
-    };
+  const handleSelect = useCallback((selected_id) => () => {
+    dispatch(GuiStore.actionCreators.selectTreeItem(selected_id));
+  }, []);
 
-  const onNodeSelect = useCallback(
-    (event: React.SyntheticEvent, nodeIds: Array<string> | string) => {
+  const drillDown = useCallback((selected_id) => () => {
+    setLevelUp(parent_id);
+    dispatch(TreeStore.actionCreators.getByParent(selected_id));
+  }, []);
 
-      console.log("onNodeSelect", nodeIds);
+  const handleChecked = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
 
-      if (typeof nodeIds === 'string') {
-        dispatch(GuiStore.actionCreators.selectTreeItem(nodeIds));
-      }
-      else{
-        dispatch(GuiStore.actionCreators.selectTreeItem(nodeIds[0]));
-      }
-    }, [])
+    var selected_id = event.target.id;
+    const currentIndex = checked.indexOf(selected_id);
+    const newChecked = [...checked];
 
-  const onNodeToggle = useCallback(
-    (event: React.SyntheticEvent, nodeIds: Array<string>) => {
-      console.log("onNodeToggle", nodeIds);
+    if (event.target.checked) {
+      newChecked.push(selected_id);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
 
-      if (nodeIds.length > 0)
-      {
-        setLevelUp(parent_id);
-        dispatch(TreeStore.actionCreators.getByParent(nodeIds[0]));
-      }
-    }, [])
+    setChecked(newChecked);
+
+    dispatch(GuiStore.actionCreators.selectTreeItem(selected_id));
+  }, []);
+
 
     return (
-      <TreeView
-        aria-label="file system navigator"
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-        sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-        onNodeSelect={onNodeSelect}
-        onNodeToggle={onNodeToggle}
-        expanded={[]}
-      >
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         {
           parent_id != null &&
-          <TreeItem nodeId={levelUp} label='UP'>
-            <TreeItem nodeId='level_up_loading' label='Loading...'>
-            </TreeItem>
-          </TreeItem>
+          <ListItem>
+            <ListItemText id={levelUp} primary='UP'>
+            </ListItemText>
+          </ListItem>
         }
         
         {
           markers?.map((marker, index) =>
-          <TreeItem nodeId={marker.id} label={marker.name}>
-            { 
-              marker.has_children &&
-                <TreeItem nodeId={marker.id + 'fake'} label='Loading...' />
-            }
-          </TreeItem>
+            <ListItem
+              key={marker.id}
+              disablePadding
+              secondaryAction={
+                marker.has_children &&
+                <IconButton edge="end" aria-label="comments">
+                  <CommentIcon onClick={drillDown(marker.id)}/>
+                </IconButton>
+              }
+            >
+              <ListItemButton role={undefined} onClick={handleSelect(marker.id)} dense>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(marker.id) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ 'aria-labelledby': marker.id }}
+                    id={marker.id}
+                    onChange={handleChecked}
+                  />
+                </ListItemIcon>
+                <ListItemText id={marker.id} primary={marker.name} />
+              </ListItemButton>
+          </ListItem>
         )}
-      </TreeView>
+      </List>
     );
 }
