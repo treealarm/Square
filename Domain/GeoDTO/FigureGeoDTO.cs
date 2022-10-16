b20b215f7a3676d4a160bc4a23986bae1a4e2f45
pruginkad <serovdanil@gmail.com>
2022-10-16 12:53:32 +0300

    feat: refactored geo operations

---
+++ Domain/GeoDTO/FigureGeoDTO.cs
@@ -0,0 +1,16 @@
+﻿using Domain.GeoDBDTO;
+using Domain.GeoDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+  public class FigureGeoDTO : FigureZoomedDTO
+  {
+    public GeometryDTO geometry { get; set; }
+    public double? radius { get; set; }    
+  }
+}
