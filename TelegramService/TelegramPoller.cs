db80d8734242cb549657c312653fe53aa95a90bc
danilserov <danil.serov2010@yandex.ru>
2023-07-31 16:29:34 +0700

    chore: codestyle

--- TelegramService/TelegramPoller.cs
+++ TelegramService/TelegramPoller.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.Text.Json;
 
 namespace TelegramService
 {
