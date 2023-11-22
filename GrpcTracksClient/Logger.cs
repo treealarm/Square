9036de373b019b3fd8f61bbb03b33f5d905aa81e
pruginkad <serovdanil@gmail.com>
2023-11-22 20:29:10 +0300

    feat: start mooving appsettings to env vars

---
+++ GrpcTracksClient/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcTracksClient
+{
+  internal class Logger
+  {
+    public static void LogException(Exception ex, string text = "", [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
+    {
+      Console.WriteLine($"{text} {ex.Message} {caller} {file}");
+    }
+  }
+}
