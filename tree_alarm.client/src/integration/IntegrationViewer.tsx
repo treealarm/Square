091bce143eb1e4c05e9322ca53950fc7399120c2
pruginkad <serovdanil@gmail.com>
2024-08-06 13:06:04 +0300

    collapsing all nested children

--- tree_alarm.client/src/integration/IntegrationViewer.tsx
+++ tree_alarm.client/src/integration/IntegrationViewer.tsx
@@ -14,25 +14,54 @@ import { useAppDispatch } from "../store/configureStore";
 import { IGetIntegrationsDTO, IIntegrationExDTO, uuidv4 } from "../store/Marker";
 
 const MUI_X_PRODUCTS: TreeViewBaseItem[] = [
-  {
-    id: '',
-    label: 'Root',
-    children: [{ id: uuidv4(), label: uuidv4() }],
-  }
+
 ];
 
+function updateTreeData(tree: TreeViewBaseItem[], parentId: string, newChildren: TreeViewBaseItem[]): TreeViewBaseItem[] {
+  return tree.map((node) => {
+    if (node.id === parentId) {
+      return { ...node, children: newChildren };
+    }
 
+    if (node.children) {
+      return { ...node, children: updateTreeData(node.children, parentId, newChildren) };
+    }
+
+    return node;
+  });
+}
+
+function getChildIds(tree: TreeViewBaseItem[], parentId: string): string[] {
+  let ids: string[] = [];
+
+  function findChildren(nodes: TreeViewBaseItem[]): void {
+    for (const node of nodes) {
+      if (node.id === parentId) {
+        // Если узел с parentId найден, собираем идентификаторы всех его детей
+        if (node.children) {
+          // Рекурсивно собираем идентификаторы детей
+          ids = node.children.flatMap(child => [child.id, ...getChildIds(node.children, child.id)]);
+        }
+        return; // Прекращаем дальнейший обход
+      }
+      // Если узел имеет дочерние элементы, продолжаем рекурсивный обход
+      if (node.children) {
+        findChildren(node.children);
+      }
+    }
+  }
+
+  findChildren(tree);
+  return ids;
+}
 
 export function IntegrationViewer() {
 
   const [data, setData] = React.useState<TreeViewBaseItem[]>(MUI_X_PRODUCTS);
+  const [expandedItems, setExpandedItems] = React.useState<string[]>([]);
 
   const appDispatch = useAppDispatch();
   const expanded_integration: IGetIntegrationsDTO = useSelector((state: ApplicationState) => state?.integrationsStates?.integrations);
-
-  function getItemId(item) {
-    return item.internalId;
-  }
 
   const transformChildren = (children: IIntegrationExDTO[]): TreeViewBaseItem[] => {    
 
@@ -53,29 +82,25 @@ export function IntegrationViewer() {
 
   useEffect(() => {
     
-    if (expanded_integration != null) {
-      setData((prevData) => {
-        
-        return prevData.map((item) => {
-          console.log("item.id:", item.id, "expanded_integration:",expanded_integration);
-          var parent_id = expanded_integration?.parent_id;
-          if(parent_id == null)
-          {
-            parent_id = '';
-          }
-          if (item.id == parent_id) {
-            return {
-              ...item,
-              children: transformChildren(expanded_integration.children),
-            };
-          } else {
-            return item;
-          }
-        });
-      });
+    if (expanded_integration) {
+      const parentId = expanded_integration.parent_id || '';
+      const newChildren = transformChildren(expanded_integration?.children);
+
+    if(data.length == 0)
+    {
+      setData(newChildren);
+      return;
+    }
+      setData((prevData) => updateTreeData(prevData, parentId, newChildren));
     }
   }, [expanded_integration]);
 
+  const handleExpandedItemsChange = (
+    event: React.SyntheticEvent,
+    itemIds: string[],
+  ) => {
+    
+  };
 
   const handleItemExpansionToggle = (
     event: React.SyntheticEvent,
@@ -84,7 +109,15 @@ export function IntegrationViewer() {
   ) => {
     if (isExpanded) {
       appDispatch(IntegrationsStore.fetchIntegrationsByParent(itemId));
-    }    
+      
+      setExpandedItems([...expandedItems, itemId]);
+    }
+    else{
+      var children = getChildIds(data, itemId);
+      var newExpandedItems =  expandedItems
+      .filter(id => !children.includes(id) && itemId != id );
+      setExpandedItems(newExpandedItems);
+    }
   };
 
   return (
@@ -92,7 +125,9 @@ export function IntegrationViewer() {
       <Box sx={{ minHeight: 352, minWidth: 250 }}>
         <RichTreeView
           items={data}
+          expandedItems={expandedItems}
           onItemExpansionToggle={handleItemExpansionToggle}
+          onExpandedItemsChange={handleExpandedItemsChange}
         />
       </Box>
     </Stack>
