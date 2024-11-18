dcfb872bf188b66ba6049b3e566d1b3688d8d83c
pruginkad <serovdanil@gmail.com>
2024-11-18 13:26:30 +0300

    value controller

---
+++ Domain/Values/ValueDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Values
+{
+  public record ValueDTO
+  {
+    public string? id { get; set; }
+    public required string owner_id { get; set; }
+    public required string name { get; set; }
+    public object? value { get; set; }
+    public object? min { get; set; }
+    public object? max { get; set; }
+  }
+}
---
+++ LeafletAlarms/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Values;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LeafletAlarms.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase  
+  {
+    static ValueDTO _value = new ValueDTO()
+    {
+        name = "test",
+        owner_id = "671f6f6f593be419bbece727"
+      };
+    // GET api/<ValuesController>/5
+    [HttpGet("{id}")]
+    public ValueDTO Get(string id)
+    {
+      return _value;
+    }
+
+    // POST api/<ValuesController>
+    [HttpPost]
+    public void Post([FromBody] ValueDTO value)
+    {
+      _value = value;
+    }
+
+
+    // DELETE api/<ValuesController>/5
+    [HttpDelete("{id}")]
+    public void Delete(string id)
+    {
+    }
+  }
+}
