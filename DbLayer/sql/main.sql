e0a35240c6b2b95f4aaf389ee39f99ff3073043c
pruginkad <serovdanil@gmail.com>
2025-04-16 15:07:46 +0300

    init_bd2

--- DbLayer/sql/main.sql
+++ DbLayer/sql/main.sql
@@ -1,6 +1,10 @@
 -- \cd 'D:/TESTS/Leaflet/DbLayer/sql'
 -- \! cd
--- \i 'D:/TESTS/Leaflet/DbLayer/sql/main.sql'
-\i 'events.sql'
-\i 'integro.sql'
+-- \i 'create_db.sql'
+-- \i 'main.sql'
+
+\connect "MapStore"
+\i 'sql_scripts/events.sql'
+\i 'sql_scripts/integro.sql'
+
 
