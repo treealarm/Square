4fc94092d70f31f99bb4ada34cdd3b14dae8e90d
pruginkad <serovdanil@gmail.com>
2025-09-30 12:33:29 +0300

    GeoService to postgres

---
+++ leaflet_dock/leaflet_data/initdb/sql_scripts/geometry.sql
@@ -0,0 +1,33 @@
+-- Нужно расширение PostGIS для работы с геометрией
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- Таблица трек-точек
+CREATE TABLE IF NOT EXISTS track_points (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    object_id UUID NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+
+    -- геометрия фигуры (Point, Polygon, LineString)
+    figure geometry(GEOMETRY, 4326) NOT NULL,
+    radius double precision,
+    zoom_level text,
+
+    -- метаданные
+    extra_props jsonb
+);
+
+-- индекс по object_id для фильтрации по объекту
+CREATE INDEX IF NOT EXISTS idx_track_points_object_id ON track_points(object_id);
+
+-- Индекс для быстрого поиска по времени
+CREATE INDEX IF NOT EXISTS idx_track_points_ts ON track_points(timestamp);
+
+-- GIN-индекс для быстрого поиска по jsonb свойствам
+CREATE INDEX IF NOT EXISTS idx_track_points_extra_props ON track_points USING gin (extra_props);
+
+-- Пример поиска по свойству в jsonb:
+-- SELECT * FROM track_points
+-- WHERE extra_props @> '[{"prop_name": "track_name", "str_val": "sad"}]';
+
+-- Индекс для гео-запросов (например ближайшие точки)
+CREATE INDEX IF NOT EXISTS idx_track_points_figure_gist ON track_points USING gist (figure);
