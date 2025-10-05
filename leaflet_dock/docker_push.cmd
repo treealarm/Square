bd1f2395625c3aa7d151067b33b2e0add84c3779
pruginkad <serovdanil@gmail.com>
2025-10-05 11:56:15 +0300

    init_files

--- leaflet_dock/docker_push.cmd
+++ leaflet_dock/docker_push.cmd
@@ -1,3 +1,4 @@
 docker push pruginkad/grpctracksclient:latest
 docker push pruginkad/leafletalarms:latest
 docker push pruginkad/blinkservice:latest
+docker push pruginkad/aasubservice:latest
--- leaflet_dock/docker_tag.cmd
+++ leaflet_dock/docker_tag.cmd
@@ -1,3 +1,4 @@
 docker tag grpctracksclient:latest pruginkad/grpctracksclient:latest
 docker tag leafletalarms:latest pruginkad/leafletalarms:latest
 docker tag blinkservice:latest pruginkad/blinkservice:latest
+docker tag aasubservice:latest pruginkad/aasubservice:latest
