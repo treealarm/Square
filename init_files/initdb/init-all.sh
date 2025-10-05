3096c3f5c63ace1fb5aac782a80845cb95da57ad
pruginkad <serovdanil@gmail.com>
2025-10-05 12:38:17 +0300

    mapstore

--- init_files/initdb/init-all.sh
+++ init_files/initdb/init-all.sh
@@ -3,19 +3,19 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo "⏳ Создание БД MapStore, если не существует..."
+echo "⏳ Создание БД mapstore, если не существует..."
 
-DB_EXISTS=$(psql -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname = 'MapStore'")
+DB_EXISTS=$(psql -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname = 'mapstore'")
 
 if [ "$DB_EXISTS" != "1" ]; then
-  echo "📦 Создаём базу данных MapStore..."
-  createdb -U "$POSTGRES_USER" -O "$POSTGRES_USER" MapStore
+  echo "📦 Создаём базу данных mapstore..."
+  createdb -U "$POSTGRES_USER" -O "$POSTGRES_USER" mapstore
 else
-  echo "✅ База MapStore уже существует."
+  echo "✅ База mapstore уже существует."
 fi
 
 
-echo "📥 Импорт структуры в MapStore из main.sql..."
+echo "📥 Импорт структуры в mapstore из main.sql..."
 cd "$SCRIPT_DIR/sql_scripts"
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -f main.sql
 
