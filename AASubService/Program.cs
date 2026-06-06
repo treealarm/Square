ca2e8b0018acfc3aab1a87ca801e2670b90ad863
danil <serovdanil@gmail.com>
2026-06-06 20:29:01 +0300

    warnings

--- AASubService/Program.cs
+++ AASubService/Program.cs
@@ -50,44 +50,4 @@ var app = builder.Build();
 
 app.MapGrpcService<ActionsServiceImpl>();
 
-_ = Task.Run(async () =>
-{
-  return;
-  try
-  {
-    //http://172.16.254.136/onvif/services
-    //var camera = await Camera.CreateAsync("172.16.254.103", 80, "admin", "en123456");
-    var camera = Camera.Create("172.16.254.136", 80, "root", "root");
-    var service = await camera.GetEventService();
-
-    if (service == null)
-    {
-      Console.WriteLine("Error.");
-      return;
-    }
-
-    service.OnEventReceived += msgs =>
-    {
-      foreach (var msg in msgs)
-      {
-        Console.WriteLine("=== Notification Received ===");
-        Console.WriteLine("Topic:");
-        foreach (var node in msg.Topic.Any)
-        {
-          Console.WriteLine(node.OuterXml);
-        }
-        Console.WriteLine("Message:");
-        Console.WriteLine(msg.Message.OuterXml);
-        Console.WriteLine("=============================");
-      }      
-    };
-
-    await service.StartReceivingAsync();
-  }
-  catch (Exception ex)
-  {
-    Console.WriteLine($"Error: {ex.Message}");
-  }
-});
-
 app.Run();
