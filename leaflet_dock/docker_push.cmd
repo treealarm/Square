5e41e7ee538ce222452332ee11378dd30575cb44
danil <serovdanil@gmail.com>
2026-06-07 20:00:40 +0300

    del no need

--- leaflet_dock/docker_push.cmd
+++
@@ -1,4 +0,0 @@
-docker push pruginkad/grpctracksclient:latest
-docker push pruginkad/leafletalarms:latest
-docker push pruginkad/blinkservice:latest
-docker push pruginkad/aasubservice:latest
--- leaflet_dock/docker_tag.cmd
+++
@@ -1,4 +0,0 @@
-docker tag grpctracksclient:latest pruginkad/grpctracksclient:latest
-docker tag leafletalarms:latest pruginkad/leafletalarms:latest
-docker tag blinkservice:latest pruginkad/blinkservice:latest
-docker tag aasubservice:latest pruginkad/aasubservice:latest
